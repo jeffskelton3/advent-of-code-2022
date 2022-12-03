aee460ae20121bc1efddc105b2d5508a94663585
Jeff Skelton <jeff@jeffskelton.net>
2022-12-02 18:51:14 -0800

    night two second round down!

--- src/night-one.py
+++ src/night-one.py
@@ -1,10 +1,9 @@
-import csv
+INPUT_FILE_PATH = "./src/night-one.txt"
 
-INPUT_FILE_PATH="./src/night-one.txt"
 
 def parse_input():
     with open(INPUT_FILE_PATH) as file:
-        lines = file.readlines() 
+        lines = file.readlines()
         collected_lines: list[list[int]] = []
         current_line: list[int] = []
         for line in lines:
@@ -17,29 +16,24 @@ def parse_input():
     return collected_lines
 
 
-def sum_collection(collected_lines: list[list[int]]): 
+def sum_collection(collected_lines: list[list[int]]):
     return list(map(lambda line: sum(line), collected_lines))
-    
+
+
 def get_top_hit(collection: list[int]):
     collection.sort(reverse=True)
     return collection[0]
+
 
 def get_top_three(collection: list[int]):
     N = 3
     collection.sort(reverse=True)
     return sum(collection[:N])
 
-top_hit = get_top_hit(
- sum_collection(
-        parse_input()
-    )
-)
 
-top_three = get_top_three(
- sum_collection(
-        parse_input()
-    )
-)
+top_hit = get_top_hit(sum_collection(parse_input()))
+
+top_three = get_top_three(sum_collection(parse_input()))
 
 print(top_hit)
 print(top_three)
--- src/night-two.py
+++ src/night-two.py
@@ -1,52 +1,80 @@
 from typing import Tuple
 import csv
 
-INPUT_FILE_PATH="./src/night-two.txt"
+INPUT_FILE_PATH = "./src/night-two.txt"
 
-def parse_input(): 
+
+def parse_input():
     with open(INPUT_FILE_PATH) as csv_file:
         data = csv.reader(csv_file, delimiter=" ")
         input_list = []
         for row in data:
             input_list.append((row[0], row[1]))
-        return input_list 
+        return input_list
+
 
 class PaperRockScissors:
-    move_score = {
-        "X": 1,
-        "Y": 2,
-        "Z": 3
-    }
+    OPPONENT_ROCK = "A"
+    OPPONENT_PAPER = "B"
+    OPPONENT_SCISSORS = "C"
 
-    outcome_of_round_score = {
-        "WIN": 6,
-        "LOSE": 0,
-        "DRAW": 3
-    }
+    ROCK = "X"
+    PAPER = "Y"
+    SCISSORS = "Z"
+
+    WIN = "Z"
+    LOSE = "X"
+    DRAW = "Y"
+
+    move_score = {ROCK: 1, PAPER: 2, SCISSORS: 3}
+
+    outcome_of_round_score = {"WIN": 6, "LOSE": 0, "DRAW": 3}
+
+    move_to_outcome_key_map = {LOSE: "LOSE", DRAW: "DRAW", WIN: "WIN"}
 
     round_outcome_map = {
-        "AX": "DRAW", #ROCK ROCK
-        "BY": "DRAW", #PAPER PAPER
-        "CZ": "DRAW", #SCISSORS SCISSORS
-        "AY": "WIN", #ROCK PAPER
-        "BZ": "WIN", #PAPER SCISSORS
-        "CX": "WIN", #SCISSORS ROCK
-        "AZ": "LOSE", #ROCK SCISSORS
-        "BX": "LOSE", #PAPER ROCK 
-        "CY": "LOSE" #SCISSORS PAPER 
-    } 
+        f"{OPPONENT_ROCK}{ROCK}": "DRAW",
+        f"{OPPONENT_PAPER}{PAPER}": "DRAW",
+        f"{OPPONENT_SCISSORS}{SCISSORS}": "DRAW",
+        f"{OPPONENT_ROCK}{PAPER}": "WIN",
+        f"{OPPONENT_PAPER}{SCISSORS}": "WIN",
+        f"{OPPONENT_SCISSORS}{ROCK}": "WIN",
+        f"{OPPONENT_ROCK}{SCISSORS}": "LOSE",
+        f"{OPPONENT_PAPER}{ROCK}": "LOSE",
+        f"{OPPONENT_SCISSORS}{PAPER}": "LOSE",
+    }
 
-    def get_score_for_round(self, opponents_move, my_move):
-        my_move_score = self.move_score[my_move] 
-        score_of_round = self.outcome_of_round_score[self.round_outcome_map[f'{opponents_move}{my_move}']]
-        return score_of_round + my_move_score
-    
+    move_needed_from_outcome_map = {
+        f"{OPPONENT_ROCK}{WIN}": PAPER,
+        f"{OPPONENT_PAPER}{WIN}": SCISSORS,
+        f"{OPPONENT_SCISSORS}{WIN}": ROCK,
+        f"{OPPONENT_ROCK}{LOSE}": SCISSORS,
+        f"{OPPONENT_PAPER}{LOSE}": ROCK,
+        f"{OPPONENT_SCISSORS}{LOSE}": PAPER,
+        f"{OPPONENT_ROCK}{DRAW}": ROCK,
+        f"{OPPONENT_PAPER}{DRAW}": PAPER,
+        f"{OPPONENT_SCISSORS}{DRAW}": SCISSORS,
+    }
+
+    def get_score_for_round(self, opponents_move, outcome):
+        outcome_key = self.move_to_outcome_key_map[outcome]
+        outcome_score = self.outcome_of_round_score[outcome_key]
+        my_move = self.move_needed_from_outcome_map[f"{opponents_move}{outcome}"]
+        my_score = self.move_score[my_move]
+        return outcome_score + my_score
+
+    # def get_score_for_round(self, opponents_move, my_move):
+    #     my_move_score = self.move_score[my_move]
+    #     score_of_round = self.outcome_of_round_score[self.round_outcome_map[f'{opponents_move}{my_move}']]
+    #     return score_of_round + my_move_score
+
     def sum_scores(self, rounds: list[Tuple[str, str]]):
         scores = []
         for round in rounds:
             score = self.get_score_for_round(round[0], round[1])
             scores.append(score)
         return sum(scores)
+
 
 prs = PaperRockScissors()
 
